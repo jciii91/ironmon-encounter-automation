758e6eb19c176b2123ce77ff49853c34f7dbeb05
john costello <jcostello711@gmail.com>
2024-11-17 14:34:49 -0500

    code cleanup for submission to Custom Code Extension library

--- EncounterHunter.lua
+++ EncounterHunter.lua
@@ -1,21 +1,12 @@
-
-
--- This is a template for a custom code extension for the Ironmon Tracker.
--- To use, first rename both this top-most function and the return value at the bottom: "CodeExtensionTemplate" -> "YourFileNameHere"
--- Then fill in each function you want to use with the code you want executed during Tracker runtime.
--- The name, author, and description attribute fields are used by the Tracker to identify this extension, please always include them.
--- You can safely remove unused functions; they won't be called.
-
 local function EncounterHunter()
-	-- bullseye icon by Shashank Singh, https://www.flaticon.com/free-icons/gps-localization
 	-- Define descriptive attributes of the custom extension that are displayed on the Tracker settings
 	local self = {}
 	self.version = "1.0"
 	self.name = "Encounter Hunter"
 	self.author = "jciii91"
 	self.description = "Triggers encounters until the desired Pokémon is found. Users can set what Pokémon and level they are hunting for. Extension ceases automatic search once it is complete."
-	self.github = "jciii91/ironmon-encounter-automation" -- Replace "MyUsername" and "ExtensionRepo" to match your GitHub repo url, if any
-	self.url = string.format("https://github.com/%s", self.github or "") -- Remove this attribute if no host website available for this extension
+	self.github = "jciii91/ironmon-encounter-automation"
+	self.url = string.format("https://github.com/%s", self.github or "")
 
 	-- EncounterHunterScreen locals --
 	local EncounterHunterScreen = {}
@@ -249,16 +240,7 @@ local function EncounterHunter()
 
 	--------------------------------------
 	-- INTENRAL TRACKER FUNCTIONS BELOW
-	-- Add any number of these below functions to your extension that you want to use.
-	-- If you don't need a function, don't add it at all; leave ommitted for faster code execution.
 	--------------------------------------
-
-	-- Executed when the user clicks the "Options" button while viewing the extension details within the Tracker's UI
-	-- Remove this function if you choose not to include a way for the user to configure options for your extension
-	-- NOTE: You'll need to implement a way to save & load changes for your extension options, similar to Tracker's Settings.ini file
-	function self.configureOptions()
-		-- [ADD CODE HERE]
-	end
 
 	-- Executed when the user clicks the "Check for Updates" button while viewing the extension details within the Tracker's UI
 	-- Returns [true, downloadUrl] if an update is available (downloadUrl auto opens in browser for user); otherwise returns [false, downloadUrl]
@@ -276,11 +258,6 @@ local function EncounterHunter()
 	-- Executed only once: When the extension is enabled by the user, and/or when the Tracker first starts up, after it loads all other required files and code
 	function self.startup()
 		InfoScreen.Buttons.LaunchEHScreen = self.Buttons.LaunchEHScreen
-	end
-
-	-- Executed only once: When the extension is disabled by the user, necessary to undo any customizations, if able
-	function self.unload()
-		-- [ADD CODE HERE]
 	end
 
 	-- Executed once every 30 frames, after most data from game memory is read in
@@ -309,24 +286,13 @@ local function EncounterHunter()
 		end
 	end
 
-	-- Executed once every 30 frames, after any battle related data from game memory is read in
-	function self.afterBattleDataUpdate()
-		-- [ADD CODE HERE]
-	end
-
 	-- Executed once every 30 frames or after any redraw event is scheduled (i.e. most button presses)
 	function self.afterRedraw()
-		if Program.currentScreen ~= InfoScreen or Battle.inBattle then
+		if Program.currentScreen ~= InfoScreen or Battle.inBattle or InfoScreen.viewScreen ~= InfoScreen.Screens.ROUTE_INFO then
 			return
 		end
 
 		Drawing.drawButton(InfoScreen.Buttons.LaunchEHScreen, Utils.calcShadowColor(Theme.COLORS["Upper box background"]))
-	end
-
-	-- Executed before a button's onClick() is processed, and only once per click per button
-	-- Param: button: the button object being clicked
-	function self.onButtonClicked(button)
-		-- [ADD CODE HERE]
 	end
 
 	-- Executed after a new battle begins (wild or trainer), and only once per battle
@@ -354,30 +320,6 @@ local function EncounterHunter()
 		if not found then
 			hunting = true
 		end
-	end
-
-	-- alternative state machine location but this is a last resort
-	-- [Bizhawk only] Executed each frame (60 frames per second)
-	-- CAUTION: Avoid unnecessary calculations here, as this can easily affect performance.
-	function self.inputCheckBizhawk()
-		-- Uncomment to use, otherwise leave commented out
-			-- local mouseInput = input.getmouse() -- lowercase 'input' pulls directly from Bizhawk API
-			-- local joypadButtons = Input.getJoypadInputFormatted() -- uppercase 'Input' uses Tracker formatted input
-		-- [ADD CODE HERE]
-	end
-
-	-- [MGBA only] Executed each frame (60 frames per second)
-	-- CAUTION: Avoid unnecessary calculations here, as this can easily affect performance.
-	function self.inputCheckMGBA()
-		-- Uncomment to use, otherwise leave commented out
-			-- local joypadButtons = Input.getJoypadInputFormatted()
-		-- [ADD CODE HERE]
-	end
-
-	-- Executed each frame of the game loop, after most data from game memory is read in but before any natural redraw events occur
-	-- CAUTION: Avoid code here if possible, as this can easily affect performance. Most Tracker updates occur at 30-frame intervals, some at 10-frame.
-	function self.afterEachFrame()
-		-- [ADD CODE HERE]
 	end
 
 	return self
